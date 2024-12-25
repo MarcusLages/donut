629796f2c12becc59e8cdfc34f235c2cb94670ba
MarcusLages <marcusvlages@gmail.com>
2024-12-25 15:34:54 -0800

    Added functions to display torus and render frame to donut.h

--- src/matrix/c/donut.c
+++ src/matrix/c/donut.c
@@ -6,6 +6,10 @@
  * Programs entry point and that does all the logic.
  */
 void main() {
-
+    display_torus();
     return EXIT_SUCCESS;
 }
+
+void display_torus() {}
+
+void render_frame(const float x_rotation_rad, const float z_rotation_rad) {}
