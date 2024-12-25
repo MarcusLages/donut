a51c1a87b2f1215d6a842069dbaf99dd35421edf
MarcusLages <marcusvlages@gmail.com>
2024-12-25 15:25:02 -0800

    Added donut.h with initial constants for donut rendering

---
+++ src/matrix/c/donut.h
@@ -0,0 +1,7 @@
+ #ifndef DONUT_H
+ #define DONUT_H
+
+ #define CIRCLE_POINT_SPACING 0.07  // Gap between points inside the circumference
+ #define TORUS_CIRCLE_SPACING 0.02  // Gap between circumferences that generate a torus
+ 
+ #endif // !DONUT_H
