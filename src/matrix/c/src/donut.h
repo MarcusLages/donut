fd209a70b48295a3a67ab5517d41b136ad515e54
MarcusLages <marcusvlages@gmail.com>
2024-12-28 18:13:03 -0800

    Restructure code inside src/matrix/c

---
+++ src/matrix/c/src/donut.h
@@ -0,0 +1,26 @@
+ #ifndef DONUT_H
+ #define DONUT_H
+
+//* Math macros/constants
+
+#define CIRCLE_POINT_SPACING 0.07  // Gap between points inside the circumference
+#define TORUS_CIRCLE_SPACING 0.02  // Gap between circumferences that generate a torus
+
+
+ //* Function Prototypes
+ 
+/**
+ * Function used to display the torus/donut.
+ */
+void display_torus();
+
+/**
+ * Function used to display a single frame of the torus/donut at a specific rotation
+ * on the x and z axis.
+ * 
+ * @param x_rotation_rad degree of rotation of the torus/donut on the x axis in radians
+ * @param y_rotation_rad degree of rotation of the torus/donut on the z axis in radians
+ */
+void render_frame(const float x_rotation_rad, const float z_rotation_rad);
+
+ #endif // !DONUT_H
