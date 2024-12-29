d7cb63794d0ff2956fe2a8eb98823b4550294a59
MarcusLages <marcusvlages@gmail.com>
2024-12-28 19:15:38 -0800

    Documented window handling and special strings in render

--- src/matrix/c/src/render.h
+++ src/matrix/c/src/render.h
@@ -5,12 +5,11 @@
 #include <unistd.h>
 
 //* Rendering macros/constants
-#define FONT_COLOUR_DEFAULT "\x1B[0m"
-#define FONT_COLOUR_GREEN   "\x1B[1;32m"
+#define FONT_COLOUR_DEFAULT "\x1B[0m"                   // Default terminal font colour
+#define FONT_COLOUR_GREEN   "\x1B[1;32m"                // Green terminal font colour
+#define CLEAR_STRING        "\033[H\033[2J\033[3J"      // String used to clear terminal
 
-#define CLEAR_STRING        "\033[H\033[2J\033[3J"
-
-#define clear_terminal()    printf(CLEAR_STRING);
+#define clear_terminal()    printf(CLEAR_STRING);       // Function to clear the terminal
 
 /**
  * Constructor that contains information about the terminal window size.
@@ -24,6 +23,11 @@ typedef struct win_size {
 } win_size;
 
 //* Function prototypes
+/**
+ * Returns a win_size struct with information about the terminal window size.
+ * 
+ * @returns structure with terminal window size
+ */
 win_size get_win_size();
 
 #endif //RENDER_H_
