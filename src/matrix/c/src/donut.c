d730b99e5fde16106055619a08507922436bf1fb
MarcusLages <marcusvlages@gmail.com>
2025-01-06 05:46:33 -0800

    Added comments and took out debug prints for plane rotation in donut.c

--- src/matrix/c/src/donut.c
+++ src/matrix/c/src/donut.c
@@ -28,7 +28,7 @@ void calculate_plane_in_rotation(char **output, const win_size window,
                                 const float x_rotation_rad, const float z_rotation_rad) {
 
     const float min_depth = 5;
-    const float window_ratio = 9.0f / 20.0f;
+    const float window_ratio = 0.65f;     // Displayable window to terminal ratio
     const float max_plane_rotation_rad = M_PI_2; //  π/2 or 45º
     
     // Formula used to find the projection ratio from the real object to the screen.
@@ -36,24 +36,29 @@ void calculate_plane_in_rotation(char **output, const win_size window,
     // of the plane on the x and y axis. This max length is then projected to fit exactly
     // half the screen height.
     const float projection_ratio = (min_depth * window_ratio * window.height) / (2 * PLANE_SIZE * sin(max_plane_rotation_rad));
-    printf("projection_ratio :>> %.2f\n", projection_ratio);
 
+    // Calculates cossines and sines to avoid frequent calculations.
     const float cos_x_rot = cos(x_rotation_rad), sin_x_rot = sin(x_rotation_rad);
     const float cos_z_rot = cos(z_rotation_rad), sin_z_rot = sin(z_rotation_rad);
 
     for(float i = -PLANE_SIZE; i < PLANE_SIZE ; i += SPACE_GAP) {
         for(float j = -PLANE_SIZE; j < PLANE_SIZE; j += SPACE_GAP) {
+            // k to complete the ijk coordinate system.
+            // Using 0 because we are using a plane, so we don't need depth
             float k = 0;
 
+            // Applying two matrix multiplications to rotate the plane on the x and z axis.
             float x =  i * cos_z_rot             + j * sin_z_rot;
             float y = -i * cos_x_rot * sin_z_rot + j * cos_x_rot * cos_z_rot + k * sin_x_rot;
             float z =  i * sin_x_rot * sin_z_rot - j * sin_x_rot * cos_z_rot + k * cos_x_rot;
 
+            // Moves to the center of the screen then calculates the projection of the shape (plane)
+            // into the screen using the depth (z) considering the min_depth as 0.
             int x_projection = ((float) window.width/2) + (x * projection_ratio) / (z + min_depth);
             int y_projection = ((float) window.height/2) + (y * projection_ratio) / (z + min_depth);
             
-            printf("%.2f, %.2f, %.2f: %d, %d\n", x, y, (z + min_depth), x_projection, y_projection);
-            
+            // Only writes character to buffer if it can be printed on the screen (if it's in boundaries
+            // of the buffer)
             if(x_projection >= 0 && y_projection >= 0)
                 output[x_projection][y_projection] = '*';
         }    
