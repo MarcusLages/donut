2e631ac0ee173d218ff8d2558c9365f846ab3daa
MarcusLages <marcusvlages@gmail.com>
2025-01-07 22:51:00 -0800

    Small TODOs and comments added

--- c/src/donut.c
+++ c/src/donut.c
@@ -91,6 +91,8 @@ void matrix_rotation(const float i, const float j, const float k,
     const float cos_x_rot = cos(x_rotation_rad), sin_x_rot = sin(x_rotation_rad);
     const float cos_z_rot = cos(z_rotation_rad), sin_z_rot = sin(z_rotation_rad);
 
+    // Applying rotation over the x and z axis to vector ijk using matrix multiplication of the rotation
+    // matrices that represent the rotations over the x and z axis.
     *x_rotated =  i * cos_z_rot             + j * sin_z_rot;
     *y_rotated = -i * cos_x_rot * sin_z_rot + j * cos_x_rot * cos_z_rot + k * sin_x_rot;
     *z_rotated =  i * sin_x_rot * sin_z_rot - j * sin_x_rot * cos_z_rot + k * cos_x_rot;
